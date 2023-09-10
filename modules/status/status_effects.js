d699dc33f27404f4c77c62740f3624448d2a98e7
Jan Weigmann <janweigmann@web.de>
2023-09-10 18:58:53 +0200

    2.1.2

--- modules/status/status_effects.js
+++ modules/status/status_effects.js
@@ -185,7 +185,7 @@ export default class DSKStatusEffects{
         if(target.documentName == "Item") {
             appliedConditions = target.effects
         } else {
-            appliedConditions = target.allApplicableEffects()
+            appliedConditions = Array.from(target.allApplicableEffects())
 
             if(!game.user.isGM)
                 appliedConditions = appliedConditions.filter(e => { return !e.getFlag("dsk", "hidePlayers") })
