887061d8ca9f834aa3e826a2639855e43a14ade0
Jan <janweigmann@web.de>
2025-07-04 15:35:16 +0200

    pause

--- modules/system/pause.js
+++ modules/system/pause.js
@@ -1,4 +1,5 @@
 Hooks.on('renderGamePause', (app, html, data, options) => {
   const img = html.querySelector('img');
   img.src = "systems/dsk/icons/backgrounds/dasbunteauge.webp"
+  html.classList.add('dskpause');
 });
